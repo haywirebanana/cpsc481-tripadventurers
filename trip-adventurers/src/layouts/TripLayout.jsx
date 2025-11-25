282e22883e0822f55ac84901156ee299bd63f9f1
sufian-tariq <sufiantariq139@gmail.com>
2025-11-25 12:04:28 -0700

    fixed the page scrolling problem
    The bottom nav bar is fixed at the bottom of phone frame now

--- trip-adventurers/src/layouts/TripLayout.jsx
+++ trip-adventurers/src/layouts/TripLayout.jsx
@@ -3,13 +3,42 @@ import BottomNav from "../components/BottomNav";
 
 export default function TripLayout() {
   return (
-    <div>
-      {/* Home button */}
-      <Link to="/trip-list"><button>Home</button></Link>
+    <div style={{
+      height: '100%',
+      display: 'flex',
+      flexDirection: 'column',
+      overflow: 'hidden'
+    }}>
+      {/* home button, fixed at top */}
+      <div style={{
+        flexShrink: 0,
+        padding: '12px',
+        borderBottom: '1px solid var(--color-black)',
+        background: 'var(--color-bg)'
+      }}>
+        <Link to="/trip-list">
+          <button>Home</button>
+        </Link>
+      </div>
 
-      <Outlet />   {/* Explore / Intinerary / Documents */}
+      <div style={{
+        flex: 1,
+        overflowY: 'auto',
+        overflowX: 'hidden',
+        minHeight: 0
+      }}>
+        <Outlet />   {/* Explore / Intinerary / Documents */}
+      </div>
 
-      <BottomNav />  {/* Only for trip pages */}
+      {/* Bottom nav fixed at bottom, to fix the scrolling problem */}
+      <div style={{
+        flexShrink: 0,
+        borderTop: '2px solid var(--color-black)',
+        background: 'var(--color-bg)',
+        padding: '12px'
+      }}>
+        <BottomNav />
+      </div>
     </div>
   );
 }
